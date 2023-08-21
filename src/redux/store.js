c82fc7b717dc7d5ced81f746802978deeab14ddb
salim-hosen <salim7cse@gmail.com>
2023-08-21 23:45:58 +0600

    redux devtools uncommented

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,13 +13,13 @@ const reducers = combineReducers({
   cart: cartReducer
 });
 
-const store = createStore(
-    reducers,
-    initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+// const store = createStore(
+//     reducers,
+//     initialState, compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ));
 
-// const store = createStore(reducers, applyMiddleware(...middleware));
+const store = createStore(reducers, applyMiddleware(...middleware));
 
 export default store;
